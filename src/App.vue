d63f4aa469f2c4b50665a8b35032a4571d89e54a
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-07-22 00:14:18 +0700

    created popup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,79 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="modal">
+      <button @click="visiblePopup=true">Налоговый вычет</button>
+    </div>
+    <v-popup
+      v-if="visiblePopup"
+      @closePopup="closePopup"
+      :visiblePopup="visiblePopup"
+    />
+  </div>
 </template>
 
 <script>
+import vPopup from "./components/ModalWindow.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    vPopup,
+  },
+  data() {
+    return {
+      visiblePopup: false,
+    };
+  },
+  methods: {
+    closePopup() {
+      this.visiblePopup = false;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: linear-gradient(
+      255.35deg,
+      #dc3131 0.83%,
+      rgba(255, 79, 79, 0) 108.93%
+    ),
+    #ff5e56;
+  height: 100vh;
+}
+.modal {
+  position: relative;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    transition: 0.5s;
+    background-color: #fff;
+    color: #000;
+  }
+  :not(:hover) {
+    transition: 0.5s;
+  }
+  :disabled,
+  [disabled] {
+    border: 1px solid #bec5cc;
+    background-color: #bec5cc;
+    color: #fff;
+    cursor: default;
+  }
+}
+button {
+  background-color: inherit;
+  padding: 16px 32px;
+  color: #fff;
+  border: 1px solid #ffffff;
+  border-radius: 6px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="popup__wrapper" ref="popup__wrapper">
+    <div class="v-popup">
+      <div class="v-popup__content">fgdhfgh</div>
+      <div class="v-popup__footer">
+        <button @click="closePopup">OK</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-popup",
+  props: {
+    visiblePopup: {
+      type: Boolean,
+    },
+  },
+  methods: {
+    closePopup() {
+      this.$emit("closePopup");
+    },
+  },
+  mounted() {
+    document.addEventListener("click", (item) => {
+      if (item.target === this.$refs["popup__wrapper"]) {
+        this.closePopup();
+      }
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+.popup__wrapper {
+  background: rgba(64, 64, 64, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  min-height: 100vh;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+}
+
+.v-popup {
+  padding: 16px;
+  position: fixed;
+  top: 50px;
+  width: 320px;
+  background: #fff;
+  box-shadow: 0 0 17px 0 #e7e7e7;
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+button {
+  margin-top: 10px;
+}
+</style>
